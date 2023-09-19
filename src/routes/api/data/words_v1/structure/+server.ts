655136bbd06fde6c5ee221181819c17eb9628a9b
nn1k4 <mihails@pm.me>
2023-09-19 18:46:36 +0300

    Added api for data base words

---
+++ src/routes/api/data/words_v1/structure/+server.ts
@@ -0,0 +1,17 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+import type { TableStructure } from '../../types';
+
+export const GET = (() => {
+	// https://localhost:5173/api/data/words_v1/structure
+
+	const data: TableStructure = {
+		columns: [
+			{ name: 'id', type: 'number' },
+			{ name: 'word', type: 'string' },
+			{ name: 'translation', type: 'string' },
+			{ name: 'frequency', type: 'number' },
+		],
+		pkColumn: 'id'
+	};
+	return json(data);
+}) satisfies RequestHandler;
