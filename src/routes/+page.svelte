b775029c08ef0e00007a329e8126f6affa0606d9
nn1k4 <mihails@pm.me>
2023-09-19 18:50:32 +0300

    Retrieving data from the database words

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-	import type { Customer } from '$lib/server/db';
+	import type { Customer, Word } from '$lib/server/db';
 	import { runQuery, runStorageQuery } from '$lib/sqlite/dataApi';
 	import { waitTillStroageReady } from '$lib/sqlite/initStorages';
 	import { onMount } from 'svelte';
-
+	
 	let storageReady = false;
 	let customers: Customer[] = [];
+	let words: Word[] = [];
+	
+
+
+
 
 	onMount(async () => {
-		await waitTillStroageReady('customers_v1'); // ждем когда хранилище будет готово
+		
+		await waitTillStroageReady('words_v1'); // ждем когда хранилище будет готово
 		storageReady = true; // хранилище готово
-		// customers = (await runQuery('select * from customers_v1 limit 10')) as Customer[];
 
+		words = (await runQuery('select * from words_v1 limit 10')) as Word[];
+
+		// customers = (await runQuery('select * from customers_v1 limit 10')) as Customer[];
+		
 		// customers = (await runStorageQuery('customers_v1',{
 		// 	searchTerm: 'Paul', limit: 2
 		// }))	as Customer[];
@@ -20,19 +29,19 @@
 		// limit: 10, offset: 30
 		// }))	as Customer[];
 
-		customers = (await runStorageQuery('customers_v1',{
-		limit: 10,
-		orderByCol: 'contact',
-		orderByDir: 'desc',
-		searchTerm: 'Paul'
-		}))	as Customer[];
+		// customers = (await runStorageQuery('customers_v1',{
+		// limit: 10,
+		// orderByCol: 'contact',
+		// orderByDir: 'desc',
+		// searchTerm: 'Paul'
+		// }))	as Customer[];
 
 		// customers = (await runStorageQuery('customers_v1', {
 		// 	orderByCol: 'contact',
 		// 	orderByDir: 'desc',
 		// 	searchTerm: 'Paul'
 		// })) as Customer[];
-		console.log(customers);
+		console.log(words);
 	});
 </script>
 
@@ -43,9 +52,11 @@
 </div>
 
 <div>
-	{#each customers as customer}
+
+
+{#each words as word}
 		<div>
-			{customer.company}: Contact: {customer.contact} - {customer.phone}
+			Word: {word.word}: translation: {word.translation} frequency: {word.frequency}
 		</div>
-	{/each}
+	{/each} 
 </div>
