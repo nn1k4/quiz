29fb380e5b210a0485664e902256249afb4ab480
nn1k4 <mihails@pm.me>
2023-09-22 23:36:14 +0300

    Added offlineChecker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,62 +3,85 @@
 	// import { runQuery, runStorageQuery } from '$lib/sqlite/dataApi';
 	// import { waitTillStroageReady } from '$lib/sqlite/initStorages';
 	// import { onMount } from 'svelte';
-	import  FlashCard  from '$lib/components/flashcard/FlashCard.svelte'
+	import FlashCard from '$lib/components/flashcard/FlashCard.svelte';
+	import InternetChecker from '$lib/components/offlineChecker/offlineChecker.svelte';
+	// Проверка Workera Offline/Online
+	const handleOfflineStatus = (event: CustomEvent<{ online: boolean }>) => {
+		if (event.detail.online) {
+			console.log('Worker: online');
+		} else {
+			console.log('Worker: offline');
+		}
+	};
+
+	const handleInternetStatus = (event: CustomEvent<{ online: boolean }>) => {
+		if (event.detail.online) {
+			console.log('Internet: available');
+		} else {
+			console.log('Internet: is not available');
+		}
+	};
+
+	const handleServerAccessStatus = (event: CustomEvent<{ online: boolean }>) => {
+		if (event.detail.online) {
+			console.log('Server: available');
+		} else {
+			console.log('Server: is not available');
+		}
+	};
 
 	// Функция для преобразования unixepoch в формат datetime
 	function convertToDatetime(unixepochTime: number) {
-
 		const datetime = new Date(unixepochTime * 1000);
-		return datetime.toLocaleString("lv-LV", { hour12: false, timeZone: "UTC" }); // Вы можете изменить это согласно вашим потребностям
+		return datetime.toLocaleString('lv-LV', { hour12: false, timeZone: 'UTC' }); // Вы можете изменить это согласно вашим потребностям
 	}
 
+	// 	onMount(async () => {
 
+	// 		await waitTillStroageReady('words_v1'); // ждем когда хранилище будет готово
+	// 		await waitTillStroageReady('stages_v1'); // ждем когда хранилище будет готово
+	// 		storageReady = true; // хранилище готово
 
-// 	onMount(async () => {
-		
-// 		await waitTillStroageReady('words_v1'); // ждем когда хранилище будет готово
-// 		await waitTillStroageReady('stages_v1'); // ждем когда хранилище будет готово
-// 		storageReady = true; // хранилище готово
+	// 		// words = (await runQuery('select * from words_v1 limit 10')) as Word[];
+	// 		words = (await runQuery(`
+	// 			SELECT *
+	// 			FROM words_v1
+	// 			WHERE next_review_date <= unixepoch(datetime('now', 'localtime'))
+	// 			OR next_review_date IS NULL
+	// 			ORDER BY frequency DESC, next_review_date ASC
+	// 			LIMIT 10;
+	// 		`)) as Word[];
 
-// 		// words = (await runQuery('select * from words_v1 limit 10')) as Word[];
-// 		words = (await runQuery(`		
-// 			SELECT *
-// 			FROM words_v1 
-// 			WHERE next_review_date <= unixepoch(datetime('now', 'localtime')) 
-// 			OR next_review_date IS NULL
-// 			ORDER BY frequency DESC, next_review_date ASC
-// 			LIMIT 10;
-// 		`)) as Word[];
+	// 		stages = (await runQuery('select * from stages_v1 limit 10')) as Stage[];
 
-// 		stages = (await runQuery('select * from stages_v1 limit 10')) as Stage[];
+	// 		// customers = (await runQuery('select * from customers_v1 limit 10')) as Customer[];
 
-// 		// customers = (await runQuery('select * from customers_v1 limit 10')) as Customer[];
-		
-// 		// customers = (await runStorageQuery('customers_v1',{
-// 		// 	searchTerm: 'Paul', limit: 2
-// 		// }))	as Customer[];
+	// 		// customers = (await runStorageQuery('customers_v1',{
+	// 		// 	searchTerm: 'Paul', limit: 2
+	// 		// }))	as Customer[];
 
-// 		// customers = (await runStorageQuery('customers_v1',{
-// 		// limit: 10, offset: 30
-// 		// }))	as Customer[];
+	// 		// customers = (await runStorageQuery('customers_v1',{
+	// 		// limit: 10, offset: 30
+	// 		// }))	as Customer[];
 
-// 		// customers = (await runStorageQuery('customers_v1',{
-// 		// limit: 10,
-// 		// orderByCol: 'contact',
-// 		// orderByDir: 'desc',
-// 		// searchTerm: 'Paul'
-// 		// }))	as Customer[];
+	// 		// customers = (await runStorageQuery('customers_v1',{
+	// 		// limit: 10,
+	// 		// orderByCol: 'contact',
+	// 		// orderByDir: 'desc',
+	// 		// searchTerm: 'Paul'
+	// 		// }))	as Customer[];
 
-// 		// customers = (await runStorageQuery('customers_v1', {
-// 		// 	orderByCol: 'contact',
-// 		// 	orderByDir: 'desc',
-// 		// 	searchTerm: 'Paul'
-// 		// })) as Customer[];
-// 		console.log(words);
-// 	});
- </script>
+	// 		// customers = (await runStorageQuery('customers_v1', {
+	// 		// 	orderByCol: 'contact',
+	// 		// 	orderByDir: 'desc',
+	// 		// 	searchTerm: 'Paul'
+	// 		// })) as Customer[];
+	// 		console.log(words);
+	// 	});
+</script>
 
 <h1 class="text-5xl text-teal-500">SvelteKit Offline SQLite</h1>
+<InternetChecker on:offline-status={handleOfflineStatus} on:internet-status={handleInternetStatus} on:server-status={handleServerAccessStatus}/>
 <FlashCard />
 <!--
 <div>
